1b24a0cb0ca1f1ba8146c7e74d640fb6fc4f1a16
ycc <1015572358@qq.com>
2022-11-15 14:49:30 +0800

    统一下拉组件

---
+++ src/views/components/selectPart.vue
@@ -0,0 +1,118 @@
+<template>
+  <div class="visual_title">
+    <Dropdown @on-click="changePart">
+      <a href="javascript:void(0)" v-html="this.MenuText"> </a>
+      <DropdownMenu slot="list">
+        <DropdownItem name="part1" :selected="MenuText == '累计办理业务笔数'"
+          >累计办理业务笔数</DropdownItem
+        >
+        <DropdownItem
+          divided
+          name="part2"
+          :selected="MenuText == '编制人员信息'"
+          >编制人员信息</DropdownItem
+        >
+        <DropdownItem
+          divided
+          name="part3"
+          :selected="MenuText == '年度业务办理量'"
+          >年度业务办理量</DropdownItem
+        >
+        <DropdownItem divided name="part6" :selected="MenuText == '政法编制'"
+          >政法编制</DropdownItem
+        >
+        <DropdownItem
+          divided
+          name="part7"
+          :selected="MenuText == '分行业事业编制及占比'"
+          >分行业事业编制及占比</DropdownItem
+        >
+        <DropdownItem
+          divided
+          name="part8"
+          :selected="MenuText == '近5年退休人员预测'"
+          >近5年退休人员预测</DropdownItem
+        >
+        <DropdownItem
+          divided
+          name="part9"
+          :selected="MenuText == '分系统编制情况'"
+          >分系统编制情况</DropdownItem
+        >
+        <DropdownItem
+          divided
+          name="part10"
+          :selected="MenuText == '问题类型分析'"
+          >问题类型分析</DropdownItem
+        >
+        <DropdownItem
+          divided
+          name="part11"
+          :selected="MenuText == '业务信息详情'"
+          >业务信息详情</DropdownItem
+        >
+      </DropdownMenu>
+    </Dropdown>
+  </div>
+</template>
+<script>
+import { Message } from "iview";
+import Sever from "@/api/selfApi";
+export default {
+  props: {
+    MenuText: String,
+    partName: String,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    changePart(val) {
+      if (val == "part1") {
+        this.MenuText = "累计办理业务笔数";
+      } else if (val == "part2") {
+        this.MenuText = "编制人员信息";
+      } else if (val == "part3") {
+        this.MenuText = "年度业务办理量";
+      } else if (val == "part6") {
+        this.MenuText = "政法编制";
+      } else if (val == "part7") {
+        this.MenuText = "分行业事业编制及占比";
+      } else if (val == "part8") {
+        this.MenuText = "近5年退休人员预测";
+      } else if (val == "part9") {
+        this.MenuText = "分系统编制情况";
+      } else if (val == "part10") {
+        this.MenuText = "问题类型分析";
+      } else if (val == "part11") {
+        this.MenuText = "业务信息详情";
+      }
+      //TODO 预留全局接口（配置菜单）后台保存选中菜单
+      // this.$emit("changePart", val);
+      let obj = {};
+      obj[this.partName] = val;
+      this.$store.dispatch("refreshMenu", obj);
+      this.saveMenu();
+    },
+    async saveMenu() {
+      await Sever.smz
+        .saveMenu(this.$store.state.menus)
+        .then((res) => {
+          if (res == "success") {
+            Message.info("保存成功");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          Message.info("保存失败");
+        });
+    },
+  },
+};
+</script>
+<style scoped>
+a {
+  color: #fff;
+  font-size: 18px;
+}
+</style>
--- src/views/components/smz/part7.vue
+++ src/views/components/smz/part7.vue
@@ -1,62 +1,6 @@
 <template>
   <div class="lineback">
-    <div class="visual_title">
-      <Dropdown @on-click="changePart">
-        <a href="javascript:void(0)" v-html="this.MenuText">
-          累计办理业务笔数
-        </a>
-        <DropdownMenu slot="list">
-          <DropdownItem name="part1" :selected="MenuText == '累计办理业务笔数'"
-            >累计办理业务笔数</DropdownItem
-          >
-          <DropdownItem
-            divided
-            name="part2"
-            :selected="MenuText == '编制人员信息'"
-            >编制人员信息</DropdownItem
-          >
-          <DropdownItem
-            divided
-            name="part3"
-            :selected="MenuText == '年度业务办理量'"
-            >年度业务办理量</DropdownItem
-          >
-          <DropdownItem divided name="part6" :selected="MenuText == '政法编制'"
-            >政法编制</DropdownItem
-          >
-          <DropdownItem
-            divided
-            name="part7"
-            :selected="MenuText == '分行业事业编制及占比'"
-            >分行业事业编制及占比</DropdownItem
-          >
-          <DropdownItem
-            divided
-            name="part8"
-            :selected="MenuText == '近5年退休人员预测'"
-            >近5年退休人员预测</DropdownItem
-          >
-          <DropdownItem
-            divided
-            name="part9"
-            :selected="MenuText == '分系统编制情况'"
-            >分系统编制情况</DropdownItem
-          >
-          <DropdownItem
-            divided
-            name="part10"
-            :selected="MenuText == '问题类型分析'"
-            >问题类型分析</DropdownItem
-          >
-          <DropdownItem
-            divided
-            name="part11"
-            :selected="MenuText == '业务信息详情'"
-            >业务信息详情</DropdownItem
-          >
-        </DropdownMenu>
-      </Dropdown>
-    </div>
+    <select-part :MenuText="MenuText" :partName="partName" />
     <div class="fhy">
       <img class="fhytopl" src="/images/u3530.svg" />
       <img class="fhytop2" src="/images/u3530.svg" />
@@ -100,46 +44,17 @@
   </div>
 </template>
 <script>
+import selectPart from "@/views/components/selectPart.vue";
 export default {
+  components: {
+    selectPart,
+  },
   props: {
     partName: String,
   },
   data() {
     return { MenuText: "分行业事业编制及占比" };
   },
-  methods: {
-    changePart(val) {
-      if (val == "part1") {
-        this.MenuText = "累计办理业务笔数";
-      } else if (val == "part2") {
-        this.MenuText = "编制人员信息";
-      } else if (val == "part3") {
-        this.MenuText = "年度业务办理量";
-      } else if (val == "part6") {
-        this.MenuText = "政法编制";
-      } else if (val == "part7") {
-        this.MenuText = "分行业事业编制及占比";
-      } else if (val == "part8") {
-        this.MenuText = "近5年退休人员预测";
-      } else if (val == "part9") {
-        this.MenuText = "分系统编制情况";
-      } else if (val == "part10") {
-        this.MenuText = "问题类型分析";
-      } else if (val == "part11") {
-        this.MenuText = "业务信息详情";
-      }
-      //TODO 预留全局接口（配置菜单）后台保存选中菜单
-      // this.$emit("changePart", val);
-      let obj = {};
-      obj[this.partName] = val;
-      this.$store.dispatch("refreshMenu", obj);
-    },
-  },
+  methods: {},
 };
 </script>
-<style scoped>
-a {
-  color: #fff;
-  font-size: 18px;
-}
-</style>
