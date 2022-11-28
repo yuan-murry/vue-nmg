15c253b3b0f8e0082f1ba4f2331c5042fc0f30c8
ycc <1015572358@qq.com>
2022-11-28 18:52:29 +0800

    轮播样式

--- src/views/components/login.vue
+++ src/views/components/login.vue
@@ -90,38 +90,139 @@
       </div>
       <div class="number" @click.stop.prevent="showMessage">
         <div>
-          <span >进入系统</span>
+          <span>进入系统</span>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-/* eslint-disable */
 import $ from "@/assets/js/login/vendor.js";
-/* eslint-enable */
+import "@/assets/js/login/jquery.fullPage.min.js";
+import "@/assets/js/login/index_slick.js";
 export default {
   mounted() {
+    /* eslint-disable */
     this.$nextTick(() => {
-      let script4 = document.createElement("script");
-      script4.type = "text/javascript";
-      script4.src = "/js/login/jquery.fullPage.min.js";
-      document.body.appendChild(script4);
-      let script3 = document.createElement("script");
-      script3.type = "text/javascript";
-      script3.src = "/js/login/index_slick.js";
-      document.body.appendChild(script3);
-      let script2 = document.createElement("script");
-      script2.type = "text/javascript";
-      script2.src = "/js/login/index.js";
-      document.body.appendChild(script2);
+      $(".index_banner").slick({
+        autoplay: true,
+        arrows: false,
+        dots: false,
+        infinite: true,
+        speed: 500,
+        autoplaySpeed: 10000,
+        pauseOnHover: false,
+        fade: true,
+        responsive: [
+          {
+            breakpoint: 992,
+            settings: {
+              dots: true,
+            },
+          },
+        ],
+      });
+      $(".index_banner").init(function (slick) {
+        $(".index_banner .item.slick-current")
+          .addClass("active")
+          .siblings()
+          .removeClass("active");
+      });
+      $(".index_banner").on("afterChange", function (slick, currentSlide) {
+        $(".index_banner .item.slick-current")
+          .addClass("active")
+          .siblings()
+          .removeClass("active");
+        var _index = $(".index_banner").slick("slickCurrentSlide");
+        $(".section1 .number span")
+          .eq(_index)
+          .addClass("active")
+          .siblings()
+          .removeClass("active");
+      });
+      $(".section1 .number span").click(function () {
+        var _index = $(this).index();
+        $(".index_banner").slick("slickGoTo", _index);
+        $(this).addClass("active").siblings().removeClass("active");
+      });
+      $(".section1 .prev").click(function () {
+        $(".index_banner").slick("slickPrev");
+      });
+      $(".section1 .next").click(function () {
+        $(".index_banner").slick("slickNext");
+      });
+
+      //导航
+      //超过一定高度导航添加类名
+      var nav = $("header"); //得到导航对象
+      var win = $(window); //得到窗口对象
+      var sc = $(document); //得到document文档对象。
+      win.scroll(function () {
+        if (sc.scrollTop() >= 100) {
+          nav.addClass("on");
+        } else {
+          nav.removeClass("on");
+        }
+      });
+
+      //移动端展开nav
+      $("#navToggle").on("click", function () {
+        $(".m_nav").addClass("open");
+      });
+      //关闭nav
+      $(".m_nav .top .closed").on("click", function () {
+        $(".m_nav").removeClass("open");
+      });
+
+      //二级导航  移动端
+      $(".m_nav .ul li").click(function () {
+        $(this).children("div.dropdown_menu").slideToggle("slow");
+        $(this).siblings("li").children(".dropdown_menu").slideUp("slow");
+      });
+
+      //全屏滚动
+      $("#index_main").fullpage({
+        navigation: true,
+        slidesNavigation: true,
+        controlArrows: false,
+        continuousHorizontal: true,
+        scrollingSpeed: 1000,
+        showActiveTooltip: true,
+        anchors: ["hero", "one", "two", "three"],
+        loopHorizontal: true,
+        afterLoad: function (anchorLink, index) {
+          if (index == 1) {
+            $("header").removeClass("on");
+          }
+          if (index == 2) {
+            $("header").addClass("on");
+            $(".section2 h3")
+              .addClass("animated fadeInUp")
+              .css("animation-delay", ".1s");
+          }
+          if (index == 3) {
+            $("header").addClass("on");
+            $(".section3 h3")
+              .addClass("animated fadeInUp")
+              .css("animation-delay", ".1s");
+          }
+          if (index == 4) {
+            $("header").addClass("on");
+            $(".section4 h3")
+              .addClass("animated fadeInUp")
+              .css("animation-delay", ".1s");
+          }
+        },
+        onLeave: function (index, direction) {},
+      });
     });
+    /* eslint-enable */
   },
-  methods:{
-showMessage(){
-    this.$Message.info('开始进入系统')
-}
-  }
+  methods: {
+    showMessage() {
+      this.$Message.info("开始进入系统");
+    },
+  },
 };
 </script>
 <style scoped>
